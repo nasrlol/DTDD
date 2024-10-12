8047672e961345f921bfd8082a44ddd0961c0a21
nsrddyn@gmail.com <nsrddyn@gmail.com>
2024-10-12 20:42:32 +0200

    Initial Commit

---
+++ DTDDTests/DTDDTests.swift
@@ -0,0 +1,17 @@
+//
+//  DTDDTests.swift
+//  DTDDTests
+//
+//  Created by nsrddyn on 12/10/2024.
+//
+
+import Testing
+@testable import DTDD
+
+struct DTDDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
