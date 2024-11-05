042ac3e76653cf49fb224c245ec9aff1735fde3e
Abdellah El Morabit <nsrddyn@gmail.com>
2024-11-05 18:44:03 +0100

    geen enkel idee wat er hier gebeuerd is lol

--- DTDD/DTDDApp.swift
+++ DTDD/DTDDApp.swift
@@ -1,17 +1,37 @@
-//
-//  DTDDApp.swift
-//  DTDD
-//
-//  Created by nsrddyn on 12/10/2024.
-//
-
 import SwiftUI
 
 @main
-struct DTDDApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+struct ToDoList: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+
+		}
+		if let zoekterm = false {
+			await.userInput(zoekterm)
+	}
+}
+
+struct ContentView: View {
+
+	@State private var searchText = ""
+
+	var body: some View {
+		NavigationStack {
+			VStack(alignment: .leading) {
+				Text("Dag liefje")
+					.font(.title)
+
+				TextField("Wat gaan we kijken?", text: $searchText)
+					.padding()
+					.background(Color(.systemGray6))
+					.cornerRadius(10)
+			}
+			.padding()
+		}
+	}
+}
+
+#Preview {
+	ContentView()
 }
